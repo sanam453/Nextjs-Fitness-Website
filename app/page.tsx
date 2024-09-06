6ec3fc1fa76ee17cb78c5e0f3ba22995cb2e9e12
sanam453 <sanam.hamza9@gmail.com>
2024-09-06 08:11:29 +0430

    Added Home Page Design

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,29 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <section>
-      <div className="text-center">
-        home page
+    <section className="px-8 mx-auto">
+      <div className="text-center grid place-items-center h-[70vh]">
+        <div>
+          <h1 className="md:text-7xl text-4xl">More Confident</h1>
+          <p className="text-center max-w-4xl mt-4 mb-10">
+            Embracing a consistent workout routine not only enhances your
+            physical appearance but also strengthens your mental resilience,
+            fostering a more assured and empowered version of yourself.
+          </p>
+          <div className="flex justify-center gap-4">
+            <Button
+              size="lg"
+              className="bg-white opacity-90 text-gray-900 hover:opacity-100 hover:bg-white transition-opacity"
+            >
+              Start Now
+            </Button>
+            <Button size="lg" className="border">
+              Download App
+            </Button>
+          </div>
+        </div>
       </div>
     </section>
   );
